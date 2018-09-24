03a07f5ccf585ef604306aa190992dd829ce564f
wwwssswwwsss <37677772+wwwssswwwsss@users.noreply.github.com>
2018-09-24 18:59:40 +0800

    Update main.py

--- hw01/main.py
+++ hw01/main.py
@@ -1,1 +1,7 @@
+for a in range(1,10):
+    for c in range(1,a):
+        print (end="       ")
+    for b in range(a,10):
+            print("{}*{}={:<2d}" .format(a,b,a*b),end=" ")
+    print (" ")
 
