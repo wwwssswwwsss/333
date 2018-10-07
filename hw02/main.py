ddffdf7a2b3de832625929caa98d23c15703c81f
wwwssswwwsss <37677772+wwwssswwwsss@users.noreply.github.com>
2018-10-07 18:37:08 +0800

    Update main.py

--- hw02/main.py
+++ hw02/main.py
@@ -2,26 +2,13 @@ import turtle
 import math
 
 def draw_polygon(aTurtle, size=50, n=3):
-    ''' 绘制正多边形
 
-    args:
-        aTurtle: turtle对象实例
-        size: int类型，正多边形的边长
-        n: int类型，是几边形        
-    '''
     for i in range(n):
         aTurtle.forward(size)
         aTurtle.left(360.0/n)
 
 def draw_n_angle(aTurtle, size=50, num=5, color=None):
-    ''' 绘制正n角形，默认为黄色
 
-    args:
-        aTurtle: turtle对象实例
-        size: int类型，正多角形的边长
-        n: int类型，是几角形    
-        color: str， 图形颜色，默认不填色
-    '''
     if color:
         aTurtle.begin_fill()
         aTurtle.fillcolor(color)
@@ -34,15 +21,7 @@ def draw_n_angle(aTurtle, size=50, num=5, color=None):
         aTurtle.end_fill()
 
 def draw_5_angle(aTurtle=None, start_pos=(0,0), end_pos=(0,10), radius=100, color=None):
-    ''' 根据起始位置、结束位置和外接圆半径画五角星
 
-    args:
-        aTurtle: turtle对象实例
-        start_pos: int的二元tuple，要画的五角星的外接圆圆心
-        end_pos: int的二元tuple，圆心指向的位置坐标点
-        radius: 五角星外接圆半径
-        color: str， 图形颜色，默认不填色    
-    '''
     aTurtle = aTurtle or turtle.Turtle()
     size = radius * math.sin(math.pi/5)/math.sin(math.pi*2/5)
     aTurtle.left(math.degrees(math.atan2(end_pos[1]-start_pos[1], end_pos[0]-start_pos[0])))
@@ -54,11 +33,7 @@ def draw_5_angle(aTurtle=None, start_pos=(0,0), end_pos=(0,10), radius=100, colo
     draw_n_angle(aTurtle, size, 5, color)
 
 def draw_5_star_flag(times=20.0):
-    ''' 绘制五星红旗
 
-    args:
-        times: 五星红旗的规格为30*20， times为倍数，默认大小为10倍， 即300*200
-    '''
     width, height = 30*times, 20*times
     # 初始化屏幕和海龟
     window = turtle.Screen()
@@ -91,4 +66,6 @@ def draw_5_star_flag(times=20.0):
 
 if __name__ == '__main__':
         draw_5_star_flag()
+
+
 
